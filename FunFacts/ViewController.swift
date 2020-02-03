8b7cdad14f2476f52ff2b54740a1f4e612b5252e
syntakks <syntakks@gmail.com>
2020-02-03 15:41:01 -0600

    Initial Commit
    First Commit. Project created.

---
+++ FunFacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FunFacts
+//
+//  Created by Stephen Wall on 2/3/20.
+//  Copyright © 2020 Stephen Wall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
